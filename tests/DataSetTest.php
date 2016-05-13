b56a4cccbe2de2df9245457de669aeb2d23b2c01
Patrick Boyd <Patrick_Boyd@Dell.com>
2016-05-13 02:53:49 +0000

    Add negative SQLDataSet Test

--- tests/DataSetTest.php
+++ tests/DataSetTest.php
@@ -72,12 +72,15 @@ class DataSetTest extends PHPUnit_Framework_TestCase
         $res = $dataSet->raw_query('CREATE TABLE tblUsers (UID int, UserName varchar(255), Password varchar(255), PersonID int);');
         $this->assertNotFalse($res);
 
-        $res = $dataSet->raw_query('CREATE VIEW vUserPerople AS SELECT tblUsers.UID, tblUsers.UserName, Persons.LastName, Persons.FirstName FROM Persons INNER JOIN tblUsers ON Persons.PersonID = tblUsers.PersonID;');
+        $res = $dataSet->raw_query('CREATE VIEW vUserPeople AS SELECT tblUsers.UID, tblUsers.UserName, Persons.LastName, Persons.FirstName FROM Persons INNER JOIN tblUsers ON Persons.PersonID = tblUsers.PersonID;');
         $this->assertNotFalse($res);
+
+        $res = $dataSet->raw_query('Test=A');
+        $this->assertFalse($res);
 
         $this->assertTrue(isset($dataSet['Persons']));
         $this->assertTrue(isset($dataSet['Users']));
-        $this->assertTrue(isset($dataSet['UserPerople']));
+        $this->assertTrue(isset($dataSet['UserPeople']));
         $this->assertFalse(isset($dataSet['Test']));
 
         try
@@ -90,6 +93,14 @@ class DataSetTest extends PHPUnit_Framework_TestCase
             $this->assertTrue(true);
         }
 
+        $dataTable = $dataSet['Users'];
+        $this->assertNotFalse($dataTable);
+        $this->assertInstanceOf('Data\SQLDataTable', $dataTable);
+
+        $dataTable = $dataSet['UserPeople'];
+        $this->assertNotFalse($dataTable);
+        $this->assertInstanceOf('Data\SQLDataTable', $dataTable);
+
         $dataTable = $dataSet['Persons'];
         $this->assertNotFalse($dataTable);
         $this->assertInstanceOf('Data\SQLDataTable', $dataTable);
@@ -100,11 +111,20 @@ class DataSetTest extends PHPUnit_Framework_TestCase
         $res = $dataTable->create(array('PersonID'=>1, 'LastName'=>'Test', 'FirstName'=>'Bob', 'Address'=>'123 Fake Street', 'City'=>'Fake Town', 'State'=>'TX'));
         $this->assertFalse($res);
 
+        $obj = new stdClass();
+        $obj->PersonID = 3;
+        $obj->LastName = 'Boyd';
+        $obj->FirstName = 'Patrick';
+        $obj->Address ='321 Fake Street';
+        $obj->City = 'Temp';
+        $res = $dataTable->create($obj);
+        $this->assertTrue($res);
+
         $res = $dataTable->create(array('PersonID'=>2, 'LastName'=>'Abc', 'FirstName'=>'123', 'Address'=>'123 Fake Street', 'City'=>'Fake Town'));
         $this->assertTrue($res);
 
         $res = $dataTable->read(false);
-        $this->assertCount(2, $res);
+        $this->assertCount(3, $res);
 
         $res = $dataTable->read(new \Data\Filter('PersonID eq 1'));
         $this->assertCount(1, $res);
@@ -118,7 +138,7 @@ class DataSetTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(2, $res[0]['PersonID']);
         $this->assertEquals('123 Fake Street', $res[0]['Address']);
 
-        $res = $dataTable->read(new \Data\Filter('PersonID eq 3'));
+        $res = $dataTable->read(new \Data\Filter('PersonID eq 4'));
         $this->assertFalse($res);
 
         $res = $dataTable->read(false, false, 1);
@@ -128,12 +148,20 @@ class DataSetTest extends PHPUnit_Framework_TestCase
         $this->assertCount(1, $res);
 
         $res = $dataTable->read(false, false, false, false, array('PersonID'=>'DESC'));
-        $this->assertCount(2, $res);
+        $this->assertCount(3, $res);
         $this->assertCount(5, $res[0]);
-        $this->assertEquals(2, $res[0]['PersonID']);
+        $this->assertEquals(3, $res[0]['PersonID']);
 
         $res = $dataTable->update(new \Data\Filter('PersonID eq 1'), array('LastName'=>'Smith'));
         $this->assertTrue($res);
+
+        $obj = new stdClass();
+        $obj->FirstName = 'Tim';
+        $res = $dataTable->update(new \Data\Filter('PersonID eq 2'), $obj);
+        $this->assertTrue($res);
+
+        $res = $dataTable->update(new \Data\Filter('PersonID eq 3'), array('LastName1'=>'Smith'));
+        $this->assertFalse($res);
 
         $res = $dataTable->read(new \Data\Filter('PersonID eq 1'));
         $this->assertCount(1, $res);
@@ -143,7 +171,10 @@ class DataSetTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($res);
 
         $res = $dataTable->read(false);
-        $this->assertCount(1, $res);
+        $this->assertCount(2, $res);
+
+        $res = $dataTable->delete(new \Data\Filter('Test eq 4'));
+        $this->assertFalse($res);
     }
 }
 /* vim: set tabstop=4 shiftwidth=4 expandtab: */
