c9f3f1675995adc406cbb2494f1df551415b30bb
Patrick Boyd <Patrick_Boyd@Dell.com>
2016-05-14 00:19:58 +0000

    Add possitive user login test

--- tests/travis/SQLAuthTest.php
+++ tests/travis/SQLAuthTest.php
@@ -19,7 +19,7 @@ class SQLAuthTest extends PHPUnit_Framework_TestCase
 
         $this->assertFalse($auth->login('test', 'test'));
 
-        $dataSet->raw_query('INSERT INTO tbluser (\'test\', \'$2a$04$B5RseAQlJOuAwZzYtSeLY.sehGtBPTlkZUuh2sv9vCwXzAAI7ribO\');');
+        $dataSet->raw_query('INSERT INTO tbluser (\'test\', \'$2a$10$KSQ0GxWYv9nUS.TmNSLYnOo/33maFdXhgQ9qU.fLsMSNwtMnwl3FS\');');
 
         $res = $auth->login('test', 'test');
         $this->assertNotFalse($res);
