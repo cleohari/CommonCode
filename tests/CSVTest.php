22c084b6f52f81f05e114cdafec9c3cb00410183
Patrick Boyd <Patrick_Boyd@Dell.com>
2016-06-03 01:08:55 +0000

    Fix Serializer

--- tests/CSVTest.php
+++ tests/CSVTest.php
@@ -10,12 +10,32 @@ class CSVTest extends PHPUnit_Framework_TestCase
         $this->assertEquals("Test1,Test2,ABC\n1,a,1\n", $data);
     }
 
+    public function testObject()
+    {
+        $serializer = new \Serialize\CSVSerializer();
+        $obj = new stdClass();
+        $obj->Test1 = 1;
+        $obj->Test2 = 'a';
+        $obj->ABC = '1';
+        $array = array($obj);
+        $data = $serializer->serializeData('text/csv', $array);
+        $this->assertEquals("Test1,Test2,ABC\n1,a,1\n", $data);
+    }
+
     public function testComma()
     {
         $serializer = new \Serialize\CSVSerializer();
         $array = array(array('Test1'=>1,'Test2,3'=>'a','ABC'=>'1,0'));
         $data = $serializer->serializeData('text/csv', $array);
         $this->assertEquals("Test1,\"Test2,3\",ABC\n1,a,\"1,0\"\n", $data);
+    }
+
+    public function testBadType()
+    {
+        $serializer = new \Serialize\CSVSerializer();
+        $array = array(array('Test1'=>1,'Test2,3'=>'a','ABC'=>'1,0'));
+        $data = $serializer->serializeData('text/json', $array);
+        $this->assertNull($data);
     }
 }
 ?>
