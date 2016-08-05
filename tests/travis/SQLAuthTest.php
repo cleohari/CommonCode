f99133f6afaf3904da1e4d47259ae123e682c22e
Patrick Boyd <Patrick_Boyd@Dell.com>
2016-08-05 02:42:44 +0000

    Add set to SQLAuthTest

--- tests/travis/SQLAuthTest.php
+++ tests/travis/SQLAuthTest.php
@@ -53,6 +53,7 @@ class SQLAuthTest extends PHPUnit_Framework_TestCase
         $user = $auth->getUserByName('test');
         $this->assertTrue($user->isInGroupNamed('test'));
         $this->assertFalse($user->isInGroupNamed('test1'));
+        $user->mail = 'test@test.com';
     }
 }
 /* vim: set tabstop=4 shiftwidth=4 expandtab: */
