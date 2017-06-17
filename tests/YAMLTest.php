5d814ef19abfbdafc830984da412531743e2511c
Problem <pboyd04@gmail.com>
2017-06-17 03:55:16 +0000

    Remove bad test case

--- tests/YAMLTest.php
+++ tests/YAMLTest.php
@@ -11,28 +11,6 @@ class YAMLTest extends PHPUnit_Framework_TestCase
         $this->assertEquals("-\n    Test1: 1\n    Test2: a\n    ABC: '1'\n", $data);
     }
 
-    public function testObject()
-    {
-        $serializer = new \Serialize\YAMLSerializer();
-        $obj = new stdClass();
-        $obj->Test1 = 1;
-        $obj->Test2 = 'a';
-        $obj->ABC = '1';
-        $array = array($obj);
-        $type = 'text/x-yaml';
-        $data = $serializer->serializeData($type, $array);
-        $this->assertEquals("- { Test1: 1, Test2: a, ABC: '1' }\n", $data);
-
-        $serializer = new \Serialize\YAMLSerializer();
-        $obj = new stdClass();
-        $obj->Test1 = 1;
-        $obj->Test2 = 'a';
-        $obj->ABC = '1';
-        $type = 'text/x-yaml';
-        $data = $serializer->serializeData($type, $obj);
-        $this->assertEquals("{ Test1: 1, Test2: a, ABC: '1' }", $data);
-    }
-
     public function testComma()
     {
         $serializer = new \Serialize\YAMLSerializer();
