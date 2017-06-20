40a9d640572235506a516ac584f05cb468f49860
Patrick Boyd <Patrick_Boyd@Dell.com>
2017-06-20 15:04:21 +0000

    Add Header test

--- tests/SerializationTest.php
+++ tests/SerializationTest.php
@@ -27,8 +27,40 @@ class SerializationTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(406, $response->getStatusCode());
     }
 
+    public function testAcceptHeader()
+    {
+        $middleware = new \Http\Rest\SerializationMiddleware();
+        $uri = \Slim\Http\Uri::createFromString('http://example.org');
+        $headers = new \Slim\Http\Headers();
+        $headers->set('Accept', 'text/csv');
+        $body = new \Slim\Http\Body(fopen('php://temp', 'r+'));
+        $request = new \Slim\Http\Request('GET', $uri, $headers, array(), array(), $body);
+        $response = new \Slim\Http\Response();
+        $response = $middleware($request, $response, $this);
+        $this->assertNotNull($response);
+        $this->assertEquals('text/csv', $response->getHeaderLine('Content-Type'));
+
+        $headers->set('Accept', 'text/x-vCard');
+        $request = new \Slim\Http\Request('GET', $uri, $headers, array(), array(), $body);
+        $response = new \Slim\Http\Response();
+        $response = $middleware($request, $response, $this);
+        $this->assertNotNull($response);
+        $this->assertEquals('text/x-vCard', $response->getHeaderLine('Content-Type'));
+
+        $headers->set('Accept', '*/*');
+        $request = new \Slim\Http\Request('GET', $uri, $headers, array(), array(), $body);
+        $response = new \Slim\Http\Response();
+        $response = $middleware($request, $response, $this);
+        $this->assertNotNull($response);
+        $this->assertEquals('application/json;charset=utf-8', $response->getHeaderLine('Content-Type'));
+    }
+
     public function __invoke($request, $response)
     {
-        return $response;
+        if($request->getAttribute('format') === 'vcard')
+        {
+            return $response->withHeader('Content-Type', 'text/x-vCard');
+        }
+        return $response->withJson(array('test'=>'a'));
     }
 }
